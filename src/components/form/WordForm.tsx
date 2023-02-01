f13e89877ce9ba1c997892567fb4146fa54a6a1a
Doyoung.Kim <dyflower@nate.com>
2023-02-01 17:42:03 +0900

    :sparkles: Feat : 단어 숨기기, 보이기, 단어별 토글 기능
    단어를 전체적으로 숨기고 나타내며 단어별로 토글이 가능하도록 구현

--- src/components/form/WordForm.tsx
+++ src/components/form/WordForm.tsx
@@ -4,7 +4,7 @@ import { FormData, WordList } from '../../types/interface';
 
 const PaperWrap = styled.div`
   margin: 0 auto;
-  background-color: palegreen;
+  background-color: var(--light-gray);
   border: 1px solid gray;
   padding: 20px;
   width: 80%;
@@ -13,7 +13,7 @@ const ListWrap = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin-top: 10px;
+  margin-top: 25px;
 `;
 
 const IndexNum = styled.div`
@@ -33,6 +33,7 @@ const MeanDiv = styled.div`
   font-size: 1.3rem;
   margin-left: 10px;
   width: 40%;
+  background-color: #cfcbcb;
 `;
 
 const RemoveBtn = styled.button`
@@ -46,12 +47,16 @@ const RemoveBtn = styled.button`
 const WordForm: React.FC = () => {
   const [formData, setFormData] = useState<FormData>({ word: '', mean: '' });
   const [wordList, setWordList] = useState<WordList>([]);
+  const [selected, setSelected] = useState<boolean[]>([]);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
   const handleList = (e: React.FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setWordList(wordList.concat([[formData.word, formData.mean]]));
+    const newSelected: boolean[] = [...selected, true];
+    setSelected(newSelected);
   };
   const removeWord = (e: React.MouseEvent<HTMLButtonElement>) => {
     const parentNode = e.currentTarget.parentElement;
@@ -60,37 +65,61 @@ const WordForm: React.FC = () => {
       setWordList(wordList.filter((_, i) => i + 1 !== parseInt(removeIndex)));
     }
   };
-
+  const handleClick = (index: number) => {
+    const newSelected = [...selected];
+    newSelected[index] = !newSelected[index];
+    setSelected(newSelected);
+  };
+  const handleHideAll = () => {
+    const newSelected = selected.map((v) => true);
+    setSelected(newSelected);
+  };
+  const handleShowAll = () => {
+    const newSelected = selected.map((v) => false);
+    setSelected(newSelected);
+  };
   return (
-    <form>
-      <div>
-        <label htmlFor='word'>단어 : </label>
-        <input type='text' id='word' name='word' value={formData.word} onChange={handleChange} />
-      </div>
-      <div>
-        <label htmlFor='mean'>뜻 : </label>
-        <input type='text' id='mean' name='mean' value={formData.mean} onChange={handleChange} />
-      </div>
-      <button type='submit' onClick={handleList}>
-        제출
-      </button>
-      <div>암기목록</div>
+    <>
+      <form>
+        <div>
+          <label htmlFor='word'>단어 : </label>
+          <input type='text' id='word' name='word' value={formData.word} onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor='mean'>뜻 : </label>
+          <input type='text' id='mean' name='mean' value={formData.mean} onChange={handleChange} />
+        </div>
+        <button type='submit' onClick={handleList}>
+          제출
+        </button>
+        <div>암기목록</div>
 
-      <PaperWrap>
-        {wordList.map((v, i) => {
-          return (
-            <ListWrap key={i}>
-              <IndexNum>{i + 1}</IndexNum>
-              <WordDiv>{v[0]}</WordDiv>
-              <MeanDiv>{v[1]}</MeanDiv>
-              <RemoveBtn type='button' onClick={removeWord}>
-                삭제
-              </RemoveBtn>
-            </ListWrap>
-          );
-        })}
-      </PaperWrap>
-    </form>
+        <button type='button' onClick={handleHideAll}>
+          전체 숨기기
+        </button>
+
+        <button type='button' onClick={handleShowAll}>
+          전체 보기
+        </button>
+
+        <PaperWrap>
+          {wordList.map((v, i) => {
+            return (
+              <ListWrap key={i}>
+                <IndexNum>{i + 1}</IndexNum>
+                <WordDiv>{v[0]}</WordDiv>
+                <MeanDiv style={{ color: selected[i] ? 'transparent' : 'black' }} onClick={() => handleClick(i)}>
+                  {v[1]}
+                </MeanDiv>
+                <RemoveBtn type='button' onClick={removeWord}>
+                  삭제
+                </RemoveBtn>
+              </ListWrap>
+            );
+          })}
+        </PaperWrap>
+      </form>
+    </>
   );
 };
 
