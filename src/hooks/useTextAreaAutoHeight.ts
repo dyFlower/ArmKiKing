ef9d773244d12aa611a4b69731ca6d9a2069ed2d
Doyoung.Kim <dyflower@nate.com>
2023-02-02 16:54:26 +0900

    :sparkles: Feat : textarea 높이 자동 조절 훅 추가
    textarea에서 내용 작성시 높이를 자동으로 설정하는 커스텀훅 추가

---
+++ src/hooks/useTextAreaAutoHeight.ts
@@ -0,0 +1,16 @@
+import { useRef, useEffect } from 'react';
+
+const useTextAreaAutoHeight = (value: any) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [value]);
+
+  return textareaRef;
+};
+
+export default useTextAreaAutoHeight;
