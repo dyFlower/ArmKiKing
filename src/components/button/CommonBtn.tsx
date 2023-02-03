91ec1678c16a0ff06a01c3c549b93d839f4548ee
Doyoung.Kim <dyflower@nate.com>
2023-02-03 20:42:09 +0900

    :lipstick: Design : 기본 버튼 디자인 수정
    기본 버튼의 디자인 수정

--- src/components/button/CommonBtn.tsx
+++ src/components/button/CommonBtn.tsx
@@ -3,18 +3,16 @@ import styled from 'styled-components';
 const CommonBtn = styled.button`
   background-color: var(--black);
   color: white;
-  padding: 12px 32px;
+  padding: 10px 30px;
   text-align: center;
-  display: inline-block;
   font-size: 16px;
-  margin: 4px 2px;
   cursor: pointer;
-  border-radius: 25px;
-  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 15px;
+  box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.05);
   transition: all 0.2s ease;
 
   &:hover {
-    background-color: var(--red);
+    background-color: #007f3f;
   }
 `;
 
