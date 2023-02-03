18554c41dff262c98ba5ab773a2845431394dadb
Doyoung.Kim <dyflower@nate.com>
2023-02-04 01:02:58 +0900

    :lipstick: Design : Sentence 페이지 디자인
    문장 암기 페이지의 기본적인 스타일을 디자인하였습니다.

--- src/components/input/senteneInputStyle.ts
+++ src/components/input/senteneInputStyle.ts
@@ -4,23 +4,29 @@ export const ParaInput = styled.textarea`
   width: 100%;
   font-size: 24px;
   overflow: visible;
-  padding: 5px;
+  padding: 10px;
   min-height: 200px;
   overflow: hidden;
+  background-color: lightblue;
+  border: 2px solid skyblue;
+  box-shadow: 5px 5px 5px lightgray;
 `;
 export const AnswerInput = styled.div`
-  border: 1px solid black;
+  border: 1px solid bllightgrayack;
   height: 40px;
   line-height: 40px;
   overflow: hidden;
   font-family: var(--font);
+  background-color: white;
 `;
 export const ShowInput = styled.div`
-  margin-top: 10px;
+  margin-top: 15px;
   width: 100%;
   overflow-wrap: break-word;
-  border: 1px solid black;
   font-family: var(--font);
-  padding: 5px;
+  padding: 10px;
   min-height: 200px;
+  background-color: lightblue;
+  border: 2px solid skyblue;
+  box-shadow: 5px 5px 5px lightgray;
 `;
--- src/components/wrapper/sentencePage/Wrapper.tsx
+++ src/components/wrapper/sentencePage/Wrapper.tsx
@@ -1,17 +1,24 @@
 import styled from 'styled-components';
 
+export const PageWrapper = styled.div`
+  margin: 0 auto;
+  width: 80%;
+  height: 100vh;
+  padding-bottom: 5%;
+  background-color: #f2ecad;
+`;
 export const BlockWrapper = styled.div`
   display: flex;
-  justify-content: center;
-  gap: 3%;
+  justify-content: space-between;
   font-size: 24px;
+  margin: 0 20px;
 `;
 export const SideWrapper = styled.div`
-  width: 40%;
+  width: 48%;
 `;
 
 export const BtnWrapper = styled.div`
   display: flex;
-  height: 50px;
-  align-items: center;
+  gap: 2%;
+  margin-bottom: 15px;
 `;
